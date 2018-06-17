f8e9083b805f7ca10846970ab168f607b427ba3a
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-06-16 22:55:36 -0700

    Added usage

---
+++ includes/ft_ssl.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ssl.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/16 12:38:07 by ivankozlov        #+#    #+#             */
+/*   Updated: 2018/06/16 12:39:20 by ivankozlov       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SLL_H
+# define FT_SLL_H
+
+# define USAGE "usage: ft_ssl command [command opts] [command args]"
+
+#endif
