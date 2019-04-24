3b3376d7f4df428179bb160d92c2752b917133d1
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-04-24 08:15:12 -0700

    Added read_chunk function

--- srcs/reader.c
+++ srcs/reader.c
@@ -1,22 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ssl.h                                           :+:      :+:    :+:   */
+/*   reader.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/16 12:38:07 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/04/24 08:06:59 by ivankozlov       ###   ########.fr       */
+/*   Created: 2019/04/24 07:11:22 by ivankozlov        #+#    #+#             */
+/*   Updated: 2019/04/24 08:07:51 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_SLL_H
-# define FT_SLL_H
+#include <unistd.h>
 
-#include "stddef.h"
+#include "strings.h"
 
-# define USAGE "usage: ft_ssl command [command opts] [command args]"
+char	*read_chunk(int fd, size_t size)
+{
+	int		ret;
+	int 	buffer[size + 1];
 
-char	*read_chunk(int fd, size_t size);
-
-#endif
+	ret = read(fd, buffer, size);
+	buffer[ret] = '\0';
+	return ft_strdup((const char *)buffer);
+}
