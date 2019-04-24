8dc942e8c882306d027e5e3d203acaeead175e35
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-04-24 08:15:34 -0700

    Removed ft_ssl, added main

--- srcs/main.c
+++ srcs/main.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ssl.c                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/16 12:48:17 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/06/16 22:22:44 by ivankozlov       ###   ########.fr       */
+/*   Created: 2019/04/24 07:55:36 by ivankozlov        #+#    #+#             */
+/*   Updated: 2019/04/24 08:00:57 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "ft_ssl.h"
-#include "libft.h"
+#include "ft_printf.h"
 
 int		main(int ac, char *av[])
 {
-	if (ac == 1)
+	int		fd;
+
+	if (ac == 2)
 	{
-		ft_printf("%s\n", USAGE);
-		exit(0);
+		fd = open(av[1], O_RDONLY);
+		ft_printf("%s\n", read_chunk(fd, 512));
+		close(fd);
 	}
-	av++;
+	return (0);
 }
