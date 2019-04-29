c1bf1019fdae99267f5f4bcb54b5b8d6c2d61273
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-04-29 12:33:34 -0700

    Moved flags under src folder

--- srcs/flags.c
+++ srcs/flags.c
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 20:32:24 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/04/29 01:38:53 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/04/29 11:33:27 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,32 @@
 #include "ftstring.h"
 #include "ft_printf.h"
 
-int				ssl_get_set_flag(int get, int set)
+int			ssl_get_set_flag(int get, int set)
 {
 	static unsigned char	flags;
+	static unsigned char	persistent = FLAG_Q | FLAG_R;
 
 	flags |= set;
-	flags &= (get | set) > 0 ? flags : 0;
+	flags &= (get | set) > 0 ? flags : persistent;
 	return (flags & get);
 }
 
-static void		ssl_parse_flag(char *arg)
+char		*ssl_parse_flag(char *arg)
 {
+	int		i;
 	int		idx;
 	int		flag;
 
-	while (*arg)
+	i = 0;
+	while (arg[++i])
 	{
-		idx = ft_strchri(MD5_FLAGS, *arg);
+		idx = ft_strchri(MD5_FLAGS, arg[i]);
 		if (idx == -1)
-			error_handler(ERR_ILLEGAL_OPTION, 1, &(*arg));
+			error_handler(ERR_ILLEGAL_OPTION, 1, &(arg[i]));
 		flag = 1 << idx;
 		ssl_get_set_flag(0, flag);
-		arg++;
+		if (flag == FLAG_S)
+			return (arg + i + 1);
 	}
-}
-
-int				ssl_parse_flags(int ac, char *av[])
-{
-	int		i;
-
-	i = -1;
-	while (++i < ac)
-	{
-		if (*av[i] != '-')
-			break ;
-		ssl_parse_flag(*(av + i) + 1);
-	}
-	return (i);
+	return (arg + i);
 }
