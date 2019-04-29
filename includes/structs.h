f59561ad565e5ba3b4aa463b1b38e840bbbe27f7
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-04-29 01:41:21 -0700

    Added generic chunk and md5 type

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 22:03:06 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/04/27 23:30:55 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/04/28 21:57:08 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define STRUCTS_H
 
 # include <stddef.h>
+
+# include "dstring.h"
 
 struct							s_md5_digest
 {
@@ -26,15 +28,15 @@ typedef struct s_md5_digest		t_md5_digest;
 
 typedef struct
 {
+	t_string					*message;
+	t_md5_digest				d;
+}								t_md5;
+
+typedef struct
+{
 	size_t						size;
 	unsigned char				*msg;
 }								t_chunk;
-
-struct							s_md5
-{
-	size_t						size;
-};
-typedef struct s_md5			t_md5;
 
 typedef							void (*t_md5_step_helper)
 	(t_md5_digest, unsigned int*, unsigned int*, int);
