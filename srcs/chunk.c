0cc7e2aaf660fe62f0833374e573c2595adc8aa9
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-02 11:17:53 -0700

    Refactored file structure

---
+++ srcs/chunk.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunk.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/02 05:28:31 by ivankozlov        #+#    #+#             */
+/*   Updated: 2019/05/02 11:15:44 by ivankozlov       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ssl.h"
+#include "structs.h"
+#include "ftstream.h"
+
+#include "ft_printf.h"
+#include "memory.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void			print_chunk(t_chunk chunk)
+{
+	int		i;
+
+	for (size_t i = 0; i < 64; i++)
+	{
+		ft_printf("%.2x%s", chunk.msg[i], i % 8 == 7 ? " " : "");
+	}
+	ft_printf("\n");
+}
+
+void			padd_chunk(t_chunk *chunk, size_t total)
+{
+	total = total * 8;
+	ft_memcpy((chunk->msg + 56), &total, sizeof(size_t));
+}
+
+t_chunk			*init_chunk(size_t size)
+{
+	t_chunk		*ret;
+
+	ret = malloc(sizeof(t_chunk));
+	ret->msg = malloc(size);
+	return (ret);
+}
+
+t_chunk		*get_chunk(int fd, size_t chunk_size)
+{
+	ssize_t			read;
+	t_chunk			*ret;
+
+	ret = init_chunk(chunk_size);
+	read = read_chunk(fd, chunk_size, (void **)&ret->msg);
+	ret->size = read;
+	if (read < 0)
+	{
+		ft_free(2, ret->msg, ret);
+		return (NULL);
+	}
+	return (ret);
+}
+
+t_chunk		*get_chunk_stream(t_stream stream, size_t chunk_size)
+{
+	t_chunk		*chunk;
+
+	if (stream.fd >= 0)
+		chunk = get_chunk(stream.fd, chunk_size);
+	else
+		chunk = get_chunk_string(stream, chunk_size);
+	if (stream.content)
+		string_append(stream.content, (char *)chunk->msg);
+	return (chunk);
+}
+
+t_chunk		*get_chunk_string(t_stream stream, size_t chunk_size)
+{
+	t_chunk			*chunk;
+
+	chunk = init_chunk(chunk_size);
+	if (stream.pos <= stream.size)
+	{
+		chunk->msg = ft_memdup(stream.string + stream.pos, chunk_size);
+		stream.pos += chunk_size;
+		chunk->size = stream.pos > stream.size
+			? stream.size % chunk_size : chunk_size;
+		ft_bzero(chunk->msg + chunk->size, chunk_size - chunk->size);
+	}
+	return (chunk);
+}
--- srcs/digest.c
+++ srcs/digest.c
@@ -6,24 +6,28 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 11:33:55 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/04/29 11:34:29 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/02 11:14:35 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_ssl.h"
+
 #include "memory.h"
-#include "structs.h"
 #include "numbers.h"
 #include "ft_printf.h"
 
-t_md5_digest	init_digest(void)
+t_digest		md5_init_digest(void)
 {
 	static const unsigned int	defaults[] = { 0x67452301, 0xefcdab89,
 		0x98badcfe, 0x10325476 };
+	t_digest					digest;
 
-	return (*(t_md5_digest *)defaults);
+	digest.size = 4;
+	digest.words = ft_memdup(defaults, sizeof(unsigned int) * 4);
+	return (digest);
 }
 
-t_md5_digest	init_digest_empty_string(void)
+t_md5_digest	md5_init_digest_empty_string(void)
 {
 	static const unsigned int	defaults[] = { 0xd98c1dd4, 0x04b2008f,
 		0x980980e9, 0x7e42f8ec };
@@ -31,22 +35,23 @@ t_md5_digest	init_digest_empty_string(void)
 	return (*(t_md5_digest *)defaults);
 }
 
-unsigned int	*digest_to_array(t_md5_digest d)
+void			ssl_print_digest(t_digest d, t_stream s)
 {
-	const unsigned int	ret[4] = { to_little_endian(d.a), to_little_endian(d.b),
-		to_little_endian(d.c), to_little_endian(d.d) };
+	size_t			i;
+	char			*quote;
+	char			*input_name;
 
-	return (ft_memdup(ret, sizeof(unsigned int) * 4));
-}
-
-void			print_digest(t_md5_digest d)
-{
-	int					i;
-	unsigned int		*arr;
-
+	input_name = s.filename ? s.filename : s.string;
+	quote = s.string ? "\"" : "";
+	if (input_name && !ssl_get_toggle_flag(FLAG_Q | FLAG_R, 0))
+		ft_printf("MD5 (%s%s%s) = ", quote, input_name, quote);
+	if (ssl_get_toggle_flag(FLAG_P, 0) && s.content)
+		ft_printf("%s", s.content->content);
 	i = -1;
-	arr = digest_to_array(d);
-	while (++i < 4)
-		ft_printf("%.8x ", arr[i]);
+	while (++i < d.size)
+		ft_printf("%.8x", to_little_endian(d.words[i]));
+	if (input_name && ssl_get_toggle_flag(FLAG_R, 0)
+		&& !ssl_get_toggle_flag(FLAG_Q, 0))
+		ft_printf(" %s%s%s", quote, input_name, quote);
 	ft_printf("\n");
 }
